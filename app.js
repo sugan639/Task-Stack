6ffb47f9f2d90edf0d7b877dcec8673edaa1ad6e
sugan639 <sugankarthick1309@gmail.com>
2023-12-22 14:10:51 +0530

    styled

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 let express = require('express');
-let app = express();
+let bodyparser = require("body-parser");
 
+let app = express();
+let tasks = [];
+app.use(bodyparser.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
 
+app.use(express.static('views/public'))
+
 app.get('/', function(req, res){
-  res.render('index', {foo: 'FOO'});
+
+  res.render('index', {foo: tasks});
+
 });
 
-app.listen(4000, function(){
+app.post('/',function(request,response){
+    var nextTask = request.body.task;
+    tasks.push(nextTask);
+    response.redirect("/");
+    console.log(nextTask);
+})
+app.listen(3000, function(){
      console.log('Server is listening on port 4000!')
 
 });
